f558e8313cdf63d8059887a0059750eb1cb004e7
Van-Duyet Le (ISLab) <lvduit08@gmail.com>
2015-04-03 14:40:10 +0700

    Fix author @id from services Author

--- public/modules/authors/services/authors.client.service.js
+++ public/modules/authors/services/authors.client.service.js
@@ -3,7 +3,7 @@
 //Authors service used to communicate Authors REST endpoints
 angular.module('authors').factory('Authors', ['$resource',
 	function($resource) {
-		return $resource('authors/:authorId', { authorId: '@_id'
+		return $resource('authors/:authorId', { authorId: '@id'
 		}, {
 			update: {
 				method: 'PUT'
