9bed938855532d32ef4fb75586773888a5c80839
Van-Duyet Le (ISLab) <lvduit08@gmail.com>
2015-04-03 14:40:44 +0700

    Fix group @id from services Group

--- public/modules/groups/services/groups.client.service.js
+++ public/modules/groups/services/groups.client.service.js
@@ -3,7 +3,7 @@
 //Groups service used to communicate Groups REST endpoints
 angular.module('groups').factory('Groups', ['$resource',
 	function($resource) {
-		return $resource('groups/:groupId', { groupId: '@_id'
+		return $resource('groups/:groupId', { groupId: '@id'
 		}, {
 			update: {
 				method: 'PUT'
