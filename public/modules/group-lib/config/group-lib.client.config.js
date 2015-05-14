04122889849ec9ae145d0278455c7c495b164581
Van-Duyet Le (ISLab) <lvduit08@gmail.com>
2015-05-14 13:23:35 +0700

    Remove Menu item and group it

---
+++ public/modules/group-lib/config/group-lib.client.config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Configuring the Articles module
+angular.module('categories').run(['Menus',
+	function(Menus) {
+		// Set top bar menu items
+		Menus.addMenuItem('topbar', 'Quản lý danh mục', 'categories', 'dropdown', '/categories(/create)?');
+		Menus.addSubMenuItem('topbar', 'categories', 'Books', 'books');
+		Menus.addSubMenuItem('topbar', 'categories', 'Authors', 'authors');
+		Menus.addSubMenuItem('topbar', 'categories', 'Publishers', 'publishers');
+		Menus.addSubMenuItem('topbar', 'categories', 'Book Categories', 'categories');
+	}
+]);
