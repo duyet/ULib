58961ae51062d6ba12436e489a2aeb6104c10ab2
Van-Duyet Le (ISLab) <lvduit08@gmail.com>
2015-05-15 18:41:10 +0700

    Backup new menu

--- public/modules/core/config/main-menu.client.config.js
+++ public/modules/core/config/main-menu.client.config.js
@@ -1,14 +1,29 @@
 'use strict';
 
-// Configuring the Articles module
-angular.module('categories').run(['Menus',
+angular.module('core').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Quản lý danh mục', 'categories', 'dropdown', '/categories(/create)?');
-		Menus.addSubMenuItem('topbar', 'categories', 'Books', 'books');
-		Menus.addSubMenuItem('topbar', 'categories', 'Authors', 'authors');
-		Menus.addSubMenuItem('topbar', 'categories', 'Publishers', 'publishers');
-		Menus.addSubMenuItem('topbar', 'categories', 'Book Categories', 'categories');
-		Menus.addSubMenuItem('topbar', 'categories', 'Book Languages', 'languages');
+		Menus.addMenuItem('topbar', 'Quản lý danh mục', 'main-categories', 'dropdown', '/categories(/create)?');
+		Menus.addSubMenuItem('topbar', 'main-categories', 'Books', 'books');
+		Menus.addSubMenuItem('topbar', 'main-categories', 'Authors', 'authors');
+		Menus.addSubMenuItem('topbar', 'main-categories', 'Publishers', 'publishers');
+		Menus.addSubMenuItem('topbar', 'main-categories', 'Book Categories', 'categories');
+		Menus.addSubMenuItem('topbar', 'main-categories', 'Book Languages', 'languages');
+		Menus.addSubMenuItem('topbar', 'main-categories', 'Các dịch vụ', 'services');
+
+		Menus.addMenuItem('topbar', 'Mượn trả', 'main-service', 'dropdown', '/categories(/create)?');
+		Menus.addSubMenuItem('topbar', 'main-service', 'Mượn sách', 'loans');
+		Menus.addSubMenuItem('topbar', 'main-service', 'Trả sách', 'returns');
+		Menus.addSubMenuItem('topbar', 'main-service', 'Tra cứu', 'searches');
+		Menus.addSubMenuItem('topbar', 'main-service', 'Dịch vụ', 'servicelogs');
+
+
+		Menus.addMenuItem('topbar', 'Hệ thống', 'main-system', 'dropdown', '/categories(/create)?');
+		Menus.addSubMenuItem('topbar', 'main-system', 'Thiết lập chung', 'settings/general');
+		Menus.addSubMenuItem('topbar', 'main-system', 'Các quy định', 'settings/library');
+
+		Menus.addMenuItem('topbar', '', 'main-roles', 'dropdown', '/categories(/create)?');
+		Menus.addSubMenuItem('topbar', 'main-roles', 'Thiết lập chung', 'settings/general');
+		Menus.addSubMenuItem('topbar', 'main-roles', 'Các quy định', 'settings/library');
 	}
 ]);
